0d7a94cb628cfa44e0576565e909888b202ab071
Prasad Gajanan Kelkar <150881439+prasadkelkar2003@users.noreply.github.com>
2026-07-03 10:14:38 +0530

    Update app.py

--- app1/app.py
+++ app1/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/app1")
 def home():
     return """<h1>This is app1!</h1>
               <img src='static/images/flask-badge-1.png'/>"""
